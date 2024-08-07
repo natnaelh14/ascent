2c5a28ea6e8d7012e79ecf653a3b4d63edbebd22
natnaelh14 <natnaelh14@github.com>
2024-08-06 21:12:33 -0400

    implement project layout

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,287 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  CalendarIcon,
+  Cog6ToothIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import Link from "next/link";
+import clsx from "clsx";
+
+const navigation = [
+  { name: "Home", href: "/home", icon: HomeIcon, current: true },
+  {
+    name: "Deposit Money",
+    href: "/deposit-money",
+    icon: DocumentDuplicateIcon,
+    current: false,
+  },
+  {
+    name: "Transfer Money",
+    href: "/transfer-money",
+    icon: UsersIcon,
+    current: false,
+  },
+  {
+    name: "Wire Transfer",
+    href: "/wire-transfer",
+    icon: FolderIcon,
+    current: false,
+  },
+  {
+    name: "Payment History",
+    href: "/payment-history",
+    icon: CalendarIcon,
+    current: false,
+  },
+];
+
+const userNavigation = [
+  { name: "Your profile", href: "/profile" },
+  { name: "Sign out", href: "#" },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div>
+      <Dialog
+        open={sidebarOpen}
+        onClose={setSidebarOpen}
+        className="relative z-50 lg:hidden"
+      >
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+        />
+
+        <div className="fixed inset-0 flex">
+          <DialogPanel
+            transition
+            className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+          >
+            <TransitionChild>
+              <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                <button
+                  type="button"
+                  onClick={() => setSidebarOpen(false)}
+                  className="-m-2.5 p-2.5"
+                >
+                  <span className="sr-only">Close sidebar</span>
+                  <XMarkIcon
+                    aria-hidden="true"
+                    className="h-6 w-6 text-white"
+                  />
+                </button>
+              </div>
+            </TransitionChild>
+            {/* Sidebar component, swap this element with another sidebar if you like */}
+            <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4 ring-1">
+              <div className="flex h-16 shrink-0 items-center gap-4">
+                <Image
+                  alt="Your Company"
+                  src="/ascent.png"
+                  className="h-8 w-auto"
+                />
+                <span className="font-bold text-white">Ascent Bank</span>
+              </div>
+              <nav className="flex flex-1 flex-col">
+                <ul className="flex flex-1 flex-col gap-y-7">
+                  <li>
+                    <ul className="-mx-2 space-y-1">
+                      {navigation.map((item) => (
+                        <li key={item.name}>
+                          <a
+                            href={item.href}
+                            className={classNames(
+                              item.current
+                                ? "bg-gray-800 text-white"
+                                : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                              "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                            )}
+                          >
+                            <item.icon
+                              aria-hidden="true"
+                              className="h-6 w-6 shrink-0"
+                            />
+                            {item.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                  <li className="mt-auto">
+                    <Link
+                      href="#"
+                      className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
+                    >
+                      <Cog6ToothIcon
+                        aria-hidden="true"
+                        className="h-6 w-6 shrink-0"
+                      />
+                      Settings
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+          <div className="flex h-16 shrink-0 items-center gap-4">
+            <Image
+              alt="Your Company"
+              src="/ascent.png"
+              className="h-8 w-auto"
+              height={32}
+              width={32}
+            />
+            <span className="font-bold text-white">Ascent Bank</span>
+          </div>
+          <nav className="flex flex-1 flex-col">
+            <ul className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul className="-mx-2 space-y-1">
+                  {navigation.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className={clsx(
+                          "text-indigo-200 hover:bg-indigo-700 hover:text-white group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                        )}
+                      >
+                        <item.icon
+                          aria-hidden="true"
+                          className="h-6 w-6 shrink-0"
+                        />
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+              <li className="mt-auto">
+                <Link
+                  href="#"
+                  className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white"
+                >
+                  <Cog6ToothIcon
+                    aria-hidden="true"
+                    className="h-6 w-6 shrink-0"
+                  />
+                  Settings
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="lg:pl-72">
+        <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
+          <button
+            type="button"
+            onClick={() => setSidebarOpen(true)}
+            className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+          </button>
+
+          {/* Separator */}
+          <div
+            aria-hidden="true"
+            className="h-6 w-px bg-gray-900/10 lg:hidden"
+          />
+
+          <div className="ml-auto">
+            <div className="flex items-center gap-x-4 lg:gap-x-6">
+              {/* Profile dropdown */}
+              <Menu as="div" className="relative">
+                <MenuButton className="-m-1.5 flex items-center p-1.5">
+                  <span className="sr-only">Open user menu</span>
+                  <Image
+                    alt=""
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    className="h-8 w-8 rounded-full bg-gray-50"
+                    height={32}
+                    width={32}
+                  />
+                  <span className="hidden lg:flex lg:items-center">
+                    <span
+                      aria-hidden="true"
+                      className="ml-4 text-sm font-semibold leading-6 text-gray-900"
+                    >
+                      Tom Cook
+                    </span>
+                    <ChevronDownIcon
+                      aria-hidden="true"
+                      className="ml-2 h-5 w-5 text-gray-400"
+                    />
+                  </span>
+                </MenuButton>
+                <MenuItems
+                  transition
+                  className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                >
+                  {userNavigation.map((item) => (
+                    <MenuItem key={item.name}>
+                      {({ focus }) => (
+                        <a
+                          href={item.href}
+                          className={clsx(
+                            focus
+                              ? "bg-indigo-700 text-white"
+                              : "text-indigo-200 hover:bg-indigo-700 hover:text-white",
+                            "block px-3 py-1 text-sm leading-6"
+                          )}
+                        >
+                          {item.name}
+                        </a>
+                      )}
+                    </MenuItem>
+                  ))}
+                </MenuItems>
+              </Menu>
+            </div>
+          </div>
+        </div>
+
+        <main className="py-10">
+          <div className="px-4 sm:px-6 lg:px-8">{children}</div>
+        </main>
+      </div>
+    </div>
+  );
+}
